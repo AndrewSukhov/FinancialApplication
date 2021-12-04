d00d7902f22f5211213fd5cb01f656257ff4ff86
AndrewSukhov <igrok-cs@yandex.ru>
2021-12-04 22:29:11 +0300

    Финансовый советник + конвертор. Ввод валюты и команд convert or advice через консоль.

--- src/Praktikum.java
+++ src/Praktikum.java
@@ -1,32 +1,60 @@
+import java.util.Scanner;
+
 class Praktikum {
     public static void main(String[] args) {
+        double rateUSD = 78;
+        double rateEUR = 85;
+        double rateJPY = 0.74;
 
-        double moneyBeforeSalary = 150000.0; // Количество денег до зарплаты
-        int daysBeforeSalary = 1;
+        Scanner scanner = new Scanner(System.in);
 
-        if (moneyBeforeSalary < 3000) {
-            System.out.println("Сегодня лучше поесть дома. Экономьте, и вы дотянете до зарплаты!");
-        } else if (moneyBeforeSalary < 10000) {
-            if (daysBeforeSalary < 10) {
-                System.out.println("Окей, пора в Макдак!");
+        System.out.println("Сколько денег у вас осталось до зарплаты?");
+        double moneyBeforeSalary = scanner.nextDouble();
+
+        System.out.println("Сколько дней до зарплаты?");
+        int daysBeforeSalary = scanner.nextInt();
+
+        System.out.println("Введите команду. Доступные команды: convert и advice.");
+        String command = scanner.next();
+
+        if (command.equals("convert")) {
+
+            System.out.println("В какую валюту хотите конвертировать рубли? Доступные варианты: USD, EUR, JPY.");
+            String currency = scanner.next(); // Считайте это значение с помощью scanner
+            if (currency.equals("USD")) {
+                System.out.println("Ваши сбережения в долларах: " + moneyBeforeSalary / rateUSD);
+            } else if (currency.equals("EUR")) {
+                System.out.println("Ваши сбережения в евро: " + moneyBeforeSalary / rateEUR);
+            } else if (currency.equals("JPY")) {
+                System.out.println("Ваши сбережения в иенах: " + moneyBeforeSalary / rateJPY);
             } else {
+                System.out.println("Валюта не поддерживается.");
+            }
+
+        } else if (command.equals("advice")) {
+            if (moneyBeforeSalary < 3000) {
                 System.out.println("Сегодня лучше поесть дома. Экономьте, и вы дотянете до зарплаты!");
-            }
-
-        } else if (moneyBeforeSalary < 30000) {
-            // Допишите условие
-            if (daysBeforeSalary < 10) {
-                System.out.println("Неплохо! Прикупите долларов и зайдите поужинать в классное место.");
+            } else if (moneyBeforeSalary < 10000) {
+                if (daysBeforeSalary < 10) {
+                    System.out.println("Окей, пора в Макдак!");
+                } else {
+                    System.out.println("Сегодня лучше поесть дома. Экономьте, и вы дотянете до зарплаты!");
+                }
+            } else if (moneyBeforeSalary < 30000) {
+                if (daysBeforeSalary < 10) {
+                    System.out.println("Неплохо! Прикупите долларов и зайдите поужинать в классное место. :)");
+                } else {
+                    System.out.println("Окей, пора в Макдак!");
+                }
             } else {
-                System.out.println("Окей, пора в Макдак!");
+                if (daysBeforeSalary < 10) {
+                    System.out.println("Отлично! Заказывайте крабов!");
+                } else {
+                    System.out.println("Неплохо! Прикупите долларов и зайдите поужинать в классное место. :)");
+                }
             }
-
         } else {
-            if (daysBeforeSalary < 10) {
-                System.out.println("Класс! Заказывайте крабов!");
-            } else {
-                System.out.println("Неплохо! Прикупите долларов и зайдите поужинать в классное место.");
-            }
+            System.out.println("Извините, такой команды пока нет.");
         }
     }
 }
