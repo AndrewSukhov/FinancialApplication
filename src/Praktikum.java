3e822c89f4b7dcdb3959c184fcaab6287b61146a
AndrewSukhov <igrok-cs@yandex.ru>
2021-12-04 22:35:13 +0300

    Финансовое приложение. Запись ежедневных расходов. Новая команда меню «3 — Ввести трату».

--- src/Praktikum.java
+++ src/Praktikum.java
@@ -2,7 +2,10 @@ import java.util.Scanner;
 
 class Praktikum {
     public static void main(String[] args) {
-        double rateUSD = 78;
+        // Ниже объявите пустой массив expenses для записи трат за неделю
+        double[] expenses = new double[7];
+
+        double rateUSD = 78.5;
         double rateEUR = 85;
         double rateJPY = 0.74;
 
@@ -14,36 +17,36 @@ class Praktikum {
         System.out.println("Сколько дней до зарплаты?");
         int daysBeforeSalary = scanner.nextInt();
 
-        while (true) {//настройте бесконечный цикл здесь
-            System.out.println("Что вы хотите сделать? ");
-            System.out.println("1 - Конвертировать валюту");
-            System.out.println("2 - Получить совет");
-            System.out.println("0 - Выход"); // Новый пункт меню, осталось только реализовать логику
+        while (true) {
+            System.out.println("Что вы хотите сделать?");
+            System.out.println("1 — Конвертировать валюту");
+            System.out.println("2 — Получить совет");
+            System.out.println("3 — Ввести трату"); // Допишите вывод нового пункта меню
+            System.out.println("0 — Выход");
 
             int command = scanner.nextInt();
 
             if (command == 1) {
-                System.out.println("В какую валюту хотите конвертировать? Доступные варианты: 1 - USD, 2 - EUR или 3 - JPY.");
+                System.out.println("Ваши сбережения: " + moneyBeforeSalary + " RUB");
+                System.out.println("В какую валюту хотите конвертировать? Доступные варианты: 1 - USD, 2 - EUR, 3 - JPY.");
                 int currency = scanner.nextInt();
-
                 if (currency == 1) {
                     System.out.println("Ваши сбережения в долларах: " + moneyBeforeSalary / rateUSD);
                 } else if (currency == 2) {
                     System.out.println("Ваши сбережения в евро: " + moneyBeforeSalary / rateEUR);
                 } else if (currency == 3) {
-                    System.out.println("Ваши сбережения в йенах: " + moneyBeforeSalary / rateJPY);
+                    System.out.println("Ваши сбережения в иенах: " + moneyBeforeSalary / rateJPY);
                 } else {
-                    System.out.println("Валюта не поддерживается.");
+                    System.out.println("Неизвестная валюта");
                 }
-
             } else if (command == 2) {
                 if (moneyBeforeSalary < 3000) {
-                    System.out.println("Сегодня лучше поесть дома. Экономьте и вы дотянете до зарплаты!");
+                    System.out.println("Сегодня лучше поесть дома. Экономьте, и вы дотянете до зарплаты!");
                 } else if (moneyBeforeSalary < 10000) {
                     if (daysBeforeSalary < 10) {
                         System.out.println("Окей, пора в Макдак!");
                     } else {
-                        System.out.println("Сегодня лучше поесть дома. Экономьте и вы дотянете до зарплаты!");
+                        System.out.println("Сегодня лучше поесть дома. Экономьте, и вы дотянете до зарплаты!");
                     }
                 } else if (moneyBeforeSalary < 30000) {
                     if (daysBeforeSalary < 10) {
@@ -58,10 +61,24 @@ class Praktikum {
                         System.out.println("Неплохо! Прикупите долларов и зайдите поужинать в классное место. :)");
                     }
                 }
-            } else if (command == 0){
+            } else if (command == 3) { // Ещё одно ветвление для обработки новой команды, допишите его условие
+                // Допишите код для печати сообщения для пользователя
+                // Текст сообщения: "За какой день вы хотите ввести трату: 1-ПН, 2-ВТ, 3-СР, 4-ЧТ, 5-ПТ, 6-СБ, 7-ВС?"
+                System.out.println("За какой день вы хотите ввести трату: 1-ПН, 2-ВТ, 3-СР, 4-ЧТ, 5-ПТ, 6-СБ, 7-ВС?");
+                // Получите из консоли день, за который пользователь хочет указать расходы
+                int day = scanner.nextInt();
+                System.out.println("Введите размер траты:");
+                // Получите из консоли значение расходов и сохраните в переменной expense
+                double expense = scanner.nextDouble();
+                // Сохраните полученное значение дневных трат в массив expenses
+                // Не забудьте прибавить новое значение к уже существующим тратам
+                expenses[day - 1] += expense;
+                System.out.println("Значение сохранено!");
+
+            } else if (command == 0) {
                 System.out.println("Выход");
                 break;
-            } else{
+            } else {
                 System.out.println("Извините, такой команды пока нет.");
             }
         }
